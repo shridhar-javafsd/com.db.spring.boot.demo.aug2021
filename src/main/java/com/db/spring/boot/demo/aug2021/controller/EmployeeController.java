cc5829d04646d26c81e71d0645a3e7dae47e1369
vamandeshmukh <vamandeshmukh@gmail.com>
2021-08-06 09:48:55 +0530

    Commit
    Commit

--- src/main/java/com/db/spring/boot/demo/aug2021/controller/EmployeeController.java
+++ src/main/java/com/db/spring/boot/demo/aug2021/controller/EmployeeController.java
@@ -2,11 +2,13 @@ package com.db.spring.boot.demo.aug2021.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.db.spring.boot.demo.aug2021.model.Employee;
+import com.db.spring.boot.demo.aug2021.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
@@ -14,18 +16,23 @@ public class EmployeeController {
 	// HTTP requests - GET, POST, PUT, DELETE
 	// CRUD operations - Retrieve, Create, Update, Delete
 
-	
 	private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
-	
-	@GetMapping("/emp")
-	public Employee emp() {
-		LOG.info("emp invoked.");
-	return new Employee(101, "Sonu", 10.5);
+
+	@Autowired
+	EmployeeService service;
+		
+	@GetMapping("/getempbyid/{id}")
+
+	public Employee getEmpById(@PathVariable("id") int id) {
+		LOG.info("getEmpById EmployeeController " + id);
+	return service.getEmployeeById(id);
 	}
 
-	@PostMapping("/addemp")
-	public Employee addEmp() {
-		LOG.info("emp invoked.");
-	return new Employee(101, "Sonu", 10.5);
-	}
+//	@PostMapping("/addemp")
+//	public Employee addEmp() {
+//		LOG.info("emp invoked.");
+//	return new Employee(101, "Sonu", 10.5);
+//	}
+	
+	
 }
---
+++ src/main/java/com/db/spring/boot/demo/aug2021/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.db.spring.boot.demo.aug2021.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.db.spring.boot.demo.aug2021.model.Employee;
+
+@Service
+public class EmployeeService {
+
+	private static Logger LOG = LoggerFactory.getLogger(EmployeeService.class);
+
+	public Employee getEmployeeById(int id) {
+		LOG.info("getEmployeeById EmployeeService");
+		return new Employee(101, "Sonu", 10.5);
+	}
+}
